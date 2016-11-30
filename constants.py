03a1ab7f4d7f9fd5b51cc56394d447b261c313d6
amustafa <abdurehman@gmail.com>
2016-11-30 23:39:56 +0100

    tune red hsv values

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ window = [640, 480]
 imp = [400, 300]
 diff = [(p-q)/2 for (p,q) in zip(window, imp)]
 
-low_red = (1, 190, 150)
+low_red = (1, 210, 170)
 high_red = (9, 254, 254)
 low_blue = (60, 150, 69)
 high_blue = (100, 254, 254)
